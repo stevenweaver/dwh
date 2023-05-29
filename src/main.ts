c25745b2978651956357dc1f83d481e5c3e5e504
Steven Weaver <sweaver@temple.edu>
2023-05-29 10:07:55 -0600

    prettifying main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import * as _ from 'underscore'
+import * as _ from "underscore";
 import * as R from "ramda";
 
-function ensureKey(n:any, key:string, value:any) {
-  if (! (key in n)) n[key] = value; 
+function ensureKey(n: any, key: string, value: any) {
+  if (!(key in n)) n[key] = value;
   return n;
 }
 
-function annotateNetwork(network:any) {
-
+function annotateNetwork(network: any) {
   // const date_to_year = (n:any) => {
   //     if (n.patient_attributes.Year == "N/A" || !n.patient_attributes.Year ) {
-  //        return null; 
+  //        return null;
   //     }
 
   //     return parseInt (n.patient_attributes.Year.substr(0,4));
@@ -18,19 +17,18 @@ function annotateNetwork(network:any) {
 
   var annotatedNetwork = network;
 
-  _.each (annotatedNetwork["Nodes"], (n) => {
-      //n.year = date_to_year(n);
-      n.degree = 0;
+  _.each(annotatedNetwork["Nodes"], (n) => {
+    //n.year = date_to_year(n);
+    n.degree = 0;
   });
-  
-  _.each (annotatedNetwork["Edges"], (e) => {
-        _.each ([e.source,e.target], (n) => {
-            annotatedNetwork["Nodes"][n].degree ++;
-        });
-    });
-  
-  return annotatedNetwork;
 
+  _.each(annotatedNetwork["Edges"], (e) => {
+    _.each([e.source, e.target], (n) => {
+      annotatedNetwork["Nodes"][n].degree++;
+    });
+  });
+
+  return annotatedNetwork;
 }
 
 /**
@@ -41,15 +39,18 @@ function annotateNetwork(network:any) {
  * @param {Boolean} randomize - Determines whether the nodes in the network will be shuffled randomly before the computation of DWH
  * @returns {Number} The DWH of the network
  */
-export default function computeDWH (network:any, binBy:any, value:any, randomize:boolean) {
-
-  if(network["trace_results"]) {
+export default function computeDWH(
+  network: any,
+  binBy: any,
+  value: any,
+  randomize: boolean
+) {
+  if (network["trace_results"]) {
     network = network["trace_results"];
 
-		if (network.Settings && network.Settings.compact_json) {
-			network = decompress(network);
-		}
-
+    if (network.Settings && network.Settings.compact_json) {
+      network = decompress(network);
+    }
   }
 
   network = annotateNetwork(network);
@@ -57,17 +58,15 @@ export default function computeDWH (network:any, binBy:any, value:any, randomize
   let nodeLabels: any[] = [];
 
   _.each(network.Nodes, (n) => {
+    let binByVal = binBy(n);
 
-		let binByVal = binBy(n);
-
-		// check if binBy is a string or an array, if it is an array, then any can match
-		if(R.is(Array, binByVal)) {
-    	nodeLabels.push(R.includes(value, binByVal));
-		} else {
-    	nodeLabels.push(binByVal === value ? true : false);
-		}
+    // check if binBy is a string or an array, if it is an array, then any can match
+    if (R.is(Array, binByVal)) {
+      nodeLabels.push(R.includes(value, binByVal));
+    } else {
+      nodeLabels.push(binByVal === value ? true : false);
+    }
     n.degree = 0;
-
   });
 
   if (randomize) {
@@ -122,9 +121,10 @@ export default function computeDWH (network:any, binBy:any, value:any, randomize
   WC /= nodesOut * nodesOut;
   WX /= nodesIn * nodesOut;
 
-  return (WM + WC - 2 * WX) / (dIn / nodesIn / nodesIn + dOut / nodesOut / nodesOut);
-
-};
+  return (
+    (WM + WC - 2 * WX) / (dIn / nodesIn / nodesIn + dOut / nodesOut / nodesOut)
+  );
+}
 
 /**
  * Computes fractions of pairwise connections between different node types in a network.
@@ -134,60 +134,55 @@ export default function computeDWH (network:any, binBy:any, value:any, randomize
  * @param {boolean} randomize - Determines whether the node labels should be randomized before computing pairwise connections.
  * @return {Array} unrolled - An array of objects representing the pairwise connections between different node types, with properties 'from', 'to', and 'count'.
  */
-export function computeFractions(network:any, bin:any, randomize:boolean) {
-
-  if(network["trace_results"]) {
-
+export function computeFractions(network: any, bin: any, randomize: boolean) {
+  if (network["trace_results"]) {
     network = network["trace_results"];
 
-		if (network.Settings && network.Settings.compact_json) {
-			network = decompress(network);
-		}
-
+    if (network.Settings && network.Settings.compact_json) {
+      network = decompress(network);
+    }
   }
 
   network = annotateNetwork(network);
 
   let nodeLabels: any[] = [];
-  
-  _.each (network["Nodes"], (n)=>{
-      nodeLabels.push (bin(n));
+
+  _.each(network["Nodes"], (n) => {
+    nodeLabels.push(bin(n));
   });
 
   if (randomize) {
-    nodeLabels = _.shuffle (nodeLabels); 
+    nodeLabels = _.shuffle(nodeLabels);
   }
-  
-  let pairwiseConnections:any = {};
 
-  _.each (network["Edges"], (e) => {
-      
-      let sourceType = nodeLabels[e.source];
-      let targetType = nodeLabels[e.target];
+  let pairwiseConnections: any = {};
 
+  _.each(network["Edges"], (e) => {
+    let sourceType = nodeLabels[e.source];
+    let targetType = nodeLabels[e.target];
 
-      let sourceDegree = network["Nodes"][e.source].degree;
-      let targetDegree = network["Nodes"][e.target].degree;
+    let sourceDegree = network["Nodes"][e.source].degree;
+    let targetDegree = network["Nodes"][e.target].degree;
 
-      ensureKey(pairwiseConnections, sourceType, {});
-      ensureKey(pairwiseConnections, targetType, {});
-      ensureKey(pairwiseConnections[sourceType], targetType, 0);
-      ensureKey(pairwiseConnections[targetType], sourceType, 0);
+    ensureKey(pairwiseConnections, sourceType, {});
+    ensureKey(pairwiseConnections, targetType, {});
+    ensureKey(pairwiseConnections[sourceType], targetType, 0);
+    ensureKey(pairwiseConnections[targetType], sourceType, 0);
 
-      pairwiseConnections[sourceType][targetType] += 1/targetDegree/sourceDegree;
-      pairwiseConnections[targetType][sourceType] += 1/sourceDegree/targetDegree;
-
+    pairwiseConnections[sourceType][targetType] +=
+      1 / targetDegree / sourceDegree;
+    pairwiseConnections[targetType][sourceType] +=
+      1 / sourceDegree / targetDegree;
   });
-    
-  let unrolled:any[] = [];
-  _.each (pairwiseConnections, (v, k) => {
-      _.each (v, (v2, k2) => {
-          unrolled.push ({'from' : k, 'to' : k2, 'count' : v2});
-      });
+
+  let unrolled: any[] = [];
+  _.each(pairwiseConnections, (v, k) => {
+    _.each(v, (v2, k2) => {
+      unrolled.push({ from: k, to: k2, count: v2 });
+    });
   });
 
   return unrolled;
-
 }
 
 /**
@@ -196,40 +191,32 @@ export function computeFractions(network:any, bin:any, randomize:boolean) {
  * @param {Object} network - A network JSON that is the result from the [HIV-TRACE](https://github.com/veg/hivtrace) package. Additionally, the results from hivtrace must be annotated using `hivnetworkannotate` from the [hivclustering](https://github.com/veg/hivclustering) package.
  * @return {Object} json - Decompressed JSON
  */
-export function decompress(json:any) {
+export function decompress(json: any) {
+  _.each(["Nodes", "Edges"], (key) => {
+    let fields = _.keys(json[key]);
+    let expanded: any[] = [];
 
-	_.each(["Nodes", "Edges"], (key) => {
+    _.each(fields, (f, idx) => {
+      let field_values = json[key][f];
+      if (!_.isArray(field_values) && "values" in field_values) {
+        //console.log ('COMPRESSED');
+        let expanded_values: any[] = [];
+        _.each(field_values["values"], (v) => {
+          expanded_values.push(field_values["keys"][v]);
+        });
+        field_values = expanded_values;
+      }
+      _.each(field_values, (fv, j) => {
+        if (idx == 0) {
+          expanded.push({});
+        }
+        expanded[j][f] = fv;
+      });
+    });
 
-		let fields = _.keys(json[key]);
-		let expanded:any[] = [];
-
-		_.each(fields, (f, idx) => {
-
-			let field_values = json[key][f];
-			if (!_.isArray(field_values) && "values" in field_values) {
-				//console.log ('COMPRESSED');
-				let expanded_values:any[] = [];
-				_.each(field_values["values"], (v) => {
-					expanded_values.push(field_values["keys"][v]);
-				});
-				field_values = expanded_values;
-			}
-			_.each(field_values, (fv, j) => {
-				if (idx == 0) {
-					expanded.push({});
-				}
-				expanded[j][f] = fv;
-			});
-
-		});
-
-		json[key] = expanded;
-
-	});
+    json[key] = expanded;
+  });
 
   json.Settings.compact_json = false;
-	return json;
-
+  return json;
 }
-
-
